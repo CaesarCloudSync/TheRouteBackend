eb8f1c96c169db524bea1fe024a07aafefb380ff
amari <amari.lawal@gmail.com>
2024-06-14 20:14:51 +0100

    signup flow works next its the main functionality

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "google_cloud_run_service" "run_service" {
   template {
     spec {
       containers {
-        image = "palondomus/btdtechconnectbe:6"
+        image = "palondomus/btdtechconnectbe:7"
       }
     }
   }
